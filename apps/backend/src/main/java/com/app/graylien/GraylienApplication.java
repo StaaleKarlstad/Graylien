4f29b5ddc5297cd141aa6f2ce7230ce9392f1094
StaaleKarlstad <107145721+StaaleKarlstad@users.noreply.github.com>
2024-10-04 11:37:25 +0200

    Initial commit backend

---
+++ apps/backend/src/main/java/com/app/graylien/GraylienApplication.java
@@ -0,0 +1,13 @@
+package com.app.graylien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraylienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraylienApplication.class, args);
+	}
+
+}
