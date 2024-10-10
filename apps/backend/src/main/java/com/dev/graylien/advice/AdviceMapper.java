0bb64785b3110a6b9889ff604ff2e9333bd6bb19
StaaleKarlstad <107145721+StaaleKarlstad@users.noreply.github.com>
2024-10-10 13:20:58 +0200

    Add NullPointerExceptions

--- apps/backend/src/main/java/com/dev/graylien/advice/AdviceMapper.java
+++ apps/backend/src/main/java/com/dev/graylien/advice/AdviceMapper.java
@@ -6,11 +6,17 @@ import org.springframework.stereotype.Component;
 public class AdviceMapper {
     
     public AdviceDTO toDTO(AdviceEntity entity){
+        if (entity==null){
+            throw new NullPointerException("Can't map null value to AdviceDTO type");
+        }
         return new AdviceDTO(entity.getId(), entity.getText());
 
     }
 
     public AdviceEntity toEntity(AdviceDTO dto){
+        if (dto==null){
+            throw new NullPointerException("Can't map null value to AdviceEntity type");
+        }
         return new AdviceEntity(dto.id(), dto.text());
     }
 }
