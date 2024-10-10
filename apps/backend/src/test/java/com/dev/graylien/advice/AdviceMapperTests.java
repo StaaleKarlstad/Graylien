dea64d228cbdb37065ca8fc97a3e3882b5a66467
StaaleKarlstad <107145721+StaaleKarlstad@users.noreply.github.com>
2024-10-10 13:21:11 +0200

    Create tests for AdviceMapper

---
+++ apps/backend/src/test/java/com/dev/graylien/advice/AdviceMapperTests.java
@@ -0,0 +1,48 @@
+package com.dev.graylien.advice;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AdviceMapperTests {
+
+    AdviceMapper adviceMapper;
+    
+    @BeforeEach
+    void setUp(){
+        adviceMapper = new AdviceMapper();
+    }
+
+    @Test
+    public void shouldMapDtoToEntity(){
+        AdviceDTO dto = new AdviceDTO(1, "Test");
+        AdviceEntity entity = adviceMapper.toEntity(dto);
+
+        Assertions.assertEquals(dto.id(), entity.getId());
+        Assertions.assertEquals(dto.text(), entity.getText());
+
+    }
+
+    @Test
+    public void shouldMapEntityToDto(){
+        AdviceEntity entity = new AdviceEntity(1, "text");
+        AdviceDTO dto = adviceMapper.toDTO(entity);
+
+        Assertions.assertEquals(entity.getId(), dto.id());
+        Assertions.assertEquals(entity.getText(), dto.text());
+    }
+
+    @Test
+    public void toDtoShouldThrowNullPointerExceptionWhenEntityIsNull(){
+        var exp = Assertions.assertThrows(NullPointerException.class, () -> adviceMapper.toDTO(null));
+        Assertions.assertEquals("Can't map null value to AdviceDTO type",exp.getMessage());
+    }
+
+    @Test
+    public void toEntityShouldThrowNullPointerExceptionWhenEntityIsNull(){
+        var exp = Assertions.assertThrows(NullPointerException.class, () -> adviceMapper.toEntity(null));
+        Assertions.assertEquals("Can't map null value to AdviceEntity type",exp.getMessage());
+    }
+}
